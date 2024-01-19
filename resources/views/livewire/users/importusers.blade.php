7726d32176c977298c6fa1da030b4173e298cca6
JHilarioOjeda <jkhruspe@gmail.com>
2024-01-19 01:42:54 -0600

    Actualización de vistas

---
+++ resources/views/livewire/users/importusers.blade.php
@@ -0,0 +1,192 @@
+<div>
+    <div class="flex">
+        <button onclick="closeimportusers()" class="bg-neutral-400 hover:bg-neutral-500 p-1 rounded-lg w-fit ml-auto my-2">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 text-white">
+                <path fill-rule="evenodd" d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
+            </svg>
+        </button>
+    </div>
+
+    <div id="divimport1" class="shadow-lg p-2 rounded">
+        <p class="text-center font-semibold my-3">Importar usuarios</p>
+        <label class="block">
+            <input onChange='showimportdiv2()' type="file" class="block w-10/12 mx-auto text-xs text-gray-500 file:me-4 file:py-2 file:px-4 file:rounded-lg file:border-0 file:text-sm file:font-semibold file:bg-neutral-400  file:text-white hover:file:bg-neutral-500 file:disabled:opacity-50 file:disabled:pointer-events-none file:text-sm">
+        </label>
+        <p class="text-center text-xs text-blue-500 my-5">Descargar formato de ejemplo</p>
+    </div>
+
+
+    <div id="divimport2" class="shadow-lg p-2 rounded hidden">
+        <p class="text-sm text-center font-semibold my-3">Actualizar estados y nuevos usuarios</p>
+        <button onclick="showimportdiv3()" class="w-fit mx-auto my-2 py-2 px-3 bg-neutral-400 hover:bg-neutral-500 text-white rounded-lg flex">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mr-2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15.91 11.672a.375.375 0 0 1 0 .656l-5.603 3.113a.375.375 0 0 1-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112Z" />
+            </svg>                                         
+            Actualizar todos
+        </button>
+        <p class="text-center text-xs my-5">Importando 117 usuarios</p>
+    
+        <div class="mx-8">
+            <div class="flex my-3">
+                <p class="flex px-1 font-semibold">
+                    <span class="text-red-600 mr-3">5</span> 
+                    Duplicados 
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-red-600 mx-2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z" />
+                    </svg>
+                </p>
+                
+                <p class="text-xs text-left">Los datos importados sobrescribirán los actuales.</p>
+            </div>
+            <div class="overflow-y-auto" style="height:21vh">
+                <table class="min-w-full divide-y text-sm">
+                    <tbody class="bg-white divide-y divide-gray-200">
+                        <tr>
+                            <td class="px-1 py-4">
+                                1343117
+                            </td>
+                            <td class="px-1 py-4">
+                                Hilario Ojeda
+                            </td>
+                            <td class="px-1 py-4">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-neutral-500">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                </svg>
+
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="px-1 py-4">
+                                1343117
+                            </td>
+                            <td class="px-1 py-4">
+                                Hilario Ojeda
+                            </td>
+                            <td class="px-1 py-4">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-neutral-500">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                </svg>
+
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="px-1 py-4">
+                                1343117
+                            </td>
+                            <td class="px-1 py-4">
+                                Hilario Ojeda
+                            </td>
+                            <td class="px-1 py-4">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-neutral-500">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                </svg>
+
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="px-1 py-4">
+                                1343117
+                            </td>
+                            <td class="px-1 py-4">
+                                Hilario Ojeda
+                            </td>
+                            <td class="px-1 py-4">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-neutral-500">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                </svg>
+
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            
+        </div>
+
+        <div class="mx-8 my-2">
+            <div class="flex my-3">
+                <p class="flex px-1 font-semibold">
+                    <span class="text-blue-600 mr-3">25</span> 
+                    Nuevos
+                </p>
+            </div>
+            <div class="overflow-y-auto" style="height:21vh">
+                <table class="min-w-full divide-y text-sm">
+                    <tbody class="bg-white divide-y divide-gray-200">
+                        <tr>
+                            <td class="px-1 py-4">
+                                1343117
+                            </td>
+                            <td class="px-1 py-4">
+                                Hilario Ojeda
+                            </td>
+                            <td class="px-1 py-4">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-neutral-500">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                </svg>
+
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="px-1 py-4">
+                                1343117
+                            </td>
+                            <td class="px-1 py-4">
+                                Hilario Ojeda
+                            </td>
+                            <td class="px-1 py-4">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-neutral-500">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                </svg>
+
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="px-1 py-4">
+                                1343117
+                            </td>
+                            <td class="px-1 py-4">
+                                Hilario Ojeda
+                            </td>
+                            <td class="px-1 py-4">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-neutral-500">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                </svg>
+
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            
+        </div>
+    </div>
+
+    <div id="divimport3" class="shadow-lg p-2 rounded hidden">
+        <p class="text-center font-semibold my-3">Actualizar estados y nuevos usuarios</p>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-24 h-24 mx-auto text-green-500 my-5">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M10.125 2.25h-4.5c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125v-9M10.125 2.25h.375a9 9 0 0 1 9 9v.375M10.125 2.25A3.375 3.375 0 0 1 13.5 5.625v1.5c0 .621.504 1.125 1.125 1.125h1.5a3.375 3.375 0 0 1 3.375 3.375M9 15l2.25 2.25L15 12" />
+        </svg>
+        <p class="text-center font-bold text-lg mb-3">Carga exitosa</p>
+
+    </div>
+    
+</div>
+
+<script>
+    function showimportdiv2(){
+        document.getElementById('divimport1').classList.add('hidden');
+        document.getElementById('divimport2').classList.remove('hidden');
+    }
+
+    function showimportdiv3(){
+        document.getElementById('divimport2').classList.add('hidden');
+        document.getElementById('divimport3').classList.remove('hidden');
+    }
+</script>
+    @push('js')
+        <script>
+
+        </script>
+    @endpush
--- resources/views/livewire/users/users.blade.php
+++ resources/views/livewire/users/users.blade.php
@@ -6,7 +6,7 @@
                 <input class="w-full lg:w-4/12 mr-3 my-2 border-gray-300 focus:border-primaryColor focus:ring focus:ring-primaryColor rounded-md shadow-sm appearance-none border rounded py-2 px-3 text-gray-700" type="text" name="" id="" placeholder="Buscar">
                 
                 <div class="flex">
-                    <button class="w-fit mr-3 my-2 py-2 px-3 bg-neutral-400 hover:bg-neutral-500 text-white rounded-lg flex">
+                    <button onclick="showimportusers()" class="w-fit mr-3 my-2 py-2 px-3 bg-neutral-400 hover:bg-neutral-500 text-white rounded-lg flex">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 mr-2">
                             <path d="M12 1.5a.75.75 0 0 1 .75.75V7.5h-1.5V2.25A.75.75 0 0 1 12 1.5ZM11.25 7.5v5.69l-1.72-1.72a.75.75 0 0 0-1.06 1.06l3 3a.75.75 0 0 0 1.06 0l3-3a.75.75 0 1 0-1.06-1.06l-1.72 1.72V7.5h3.75a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3h-9a3 3 0 0 1-3-3v-9a3 3 0 0 1 3-3h3.75Z" />
                         </svg>
@@ -54,10 +54,41 @@
                                 <tbody class="bg-white divide-y divide-gray-200">
                                     <tr>
                                         <td class="px-4 py-4">
-                                            1343117
+                                            13431
                                         </td>
                                         <td class="px-4 py-4">
                                             Hilario Ojeda
+                                       </td>
+                                        <td class="px-4 py-4">
+                                            Lider   
+                                        </td>
+                                        <td class="px-4 py-4">
+                                            Operador
+                                        </td>
+                                        <td class="px-4 py-4">
+                                            Ball joint
+                                        </td>
+                                        <td class="px-4 py-4">
+                                            <button onclick="showeditusers()" class="bg-cyan-700 p-2 rounded-lg mx-2"> 
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 text-white">
+                                                    <path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32l8.4-8.4z" />
+                                                    <path d="M5.25 5.25a3 3 0 00-3 3v10.5a3 3 0 003 3h10.5a3 3 0 003-3V13.5a.75.75 0 00-1.5 0v5.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5V8.25a1.5 1.5 0 011.5-1.5h5.25a.75.75 0 000-1.5H5.25z" />
+                                                </svg>
+                                            </button>
+                                            <button class="bg-red-500 p-2 rounded-lg mx-2"> 
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 text-white">
+                                                    <path fill-rule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z" clip-rule="evenodd" />
+                                                </svg>
+                                            </button>
+
+                                       </td>
+                                    </tr>
+                                    <tr>
+                                        <td class="px-4 py-4">
+                                            34347
+                                        </td>
+                                        <td class="px-4 py-4">
+                                            Mayte del Angel
                                        </td>
                                         <td class="px-4 py-4">
                                             Lider   
@@ -98,6 +129,10 @@
         <div id="diveditusers" class="w-38p hidden ml-auto p-5 bg-white rounded shadow-lg">
             <livewire:users.usersedit/>
         </div>
+
+        <div id="divimportusers" class="w-38p hidden ml-auto p-5 bg-white rounded shadow-lg">
+            <livewire:users.importusers/>
+        </div>
     </div>
 </div>
 
@@ -106,6 +141,7 @@
         function showcreateusers()
         {
             closeeditusers();
+            closeimportusers()
             document.getElementById('divcreateusers').classList.remove("hidden");
             document.getElementById('divusers').classList.remove("w-full");
             document.getElementById('divusers').classList.add("w-60p");
@@ -121,6 +157,7 @@
         function showeditusers()
         {
             closecreateusers()
+            closeimportusers()
             document.getElementById('diveditusers').classList.remove("hidden");
             document.getElementById('divusers').classList.remove("w-full");
             document.getElementById('divusers').classList.add("w-60p");
@@ -131,6 +168,26 @@
             document.getElementById('diveditusers').classList.add("hidden");
             document.getElementById('divusers').classList.add("w-full");
             document.getElementById('divusers').classList.remove("w-60p");
+        }
+
+        function showimportusers()
+        {
+            closecreateusers()
+            closeeditusers();
+            document.getElementById('divimportusers').classList.remove("hidden");
+            document.getElementById('divusers').classList.remove("w-full");
+            document.getElementById('divusers').classList.add("w-60p");
+        }
+
+        function closeimportusers()
+        {
+            document.getElementById('divimportusers').classList.add("hidden");
+            document.getElementById('divusers').classList.add("w-full");
+            document.getElementById('divusers').classList.remove("w-60p");
+
+            document.getElementById('divimport1').classList.remove('hidden');
+            document.getElementById('divimport2').classList.add('hidden');
+            document.getElementById('divimport3').classList.add('hidden');
         }
 
     </script>
